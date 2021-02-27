3e2c5296f9cfdbfe609d154c4fcceb7547198772
June Naruworn <naruwornv@gmail.com>
2021-02-27 23:38:21 +0700

    การบ้าน Java

---
+++ ๋JavaScript/basic part2/แบบฝึกหัดพิเศษ/03.js
@@ -0,0 +1,9 @@
+function draw(n){
+    let star = ''
+    for(let i = 0; i < n;i++){
+        star = star + (1+i)
+    }
+ for(let i = 0 ; i< n ; i++)
+    console.log(star);
+    
+}
---
+++ ๋JavaScript/basic part2/แบบฝึกหัดพิเศษ/04.js
@@ -0,0 +1,11 @@
+function draw(n){
+    let star = ''
+    for(let i = 0; i < n;i++){
+        star = star +  n
+    }
+ for(let i = 0 ; i< n ; i++)
+        star = i
+    console.log(star);
+  
+
+}
